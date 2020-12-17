32127803908a57082afbb5ba54fb7b29c17a2b9d
patricklapgar <patricklapgardeveloper@gmail.com>
2020-12-17 12:05:41 -0600

    Begun OOP lesson

--- OOP Pt1/oop_part1.py
+++ OOP Pt1/oop_part1.py
@@ -22,3 +22,30 @@ class Dog:
 pitbull = Dog() # <-- This is an object. See, it's a variable that uses the class we make
 pitbull.bark() # Prints "Bark"
 
+### ABSTRACTION AND ENCAPSULATION (Big Topics!!)
+# Remember: The reason why OOP is used is because it makes writing simpler and more organized
+
+# With object oriented programming, the end goal is to build your code
+# in a logical, organized manner. You do this by putting your classes into hierarchical groups
+
+# Now on to the good stuff! Encapsulation is basically when both public and 'private' methods 
+# are grouped together into one single class. 
+# I mentioned both public and 'private' methods. By that, I mean private methods are methods that are 
+# intended only for the class you build. They won't and shouldn't be accessed by you, the programmer.
+# Encapsulation is very useful in that it allows for abstraction
+
+# Now, abstraction is when you build a class, but you expose the relevant functions for the programmer to use and you hide the private methods.
+# You would only hide private methods
+
+# Here's an example of everything I've just talked about!
+# Example: User class
+
+class User:
+#   This is the __init__ function.
+#
+#       |   |   |
+#       V   V   V
+    def __init__(self, username, password):
+        pass
+
+user1 = User()
